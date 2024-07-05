2a9550b1407b488bf61062671e88ad4c53871e6c
akashsingh <akashsinghas9504@gmail.com>
2024-07-06 02:57:19 +0530

    added qualification section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
+import Testimonials from './components/testimonials/Testimonials';
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
       <About />
       <Skills />
       <Services />
+      <Qualification />
+      <Testimonials />
     </main>
     </>
   );
